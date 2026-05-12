99e3aefaae5fd33b891c985985dda470c457b69f
omar-gaal <omargaal123@gmail.com>
2026-05-12 20:35:35 +0200

    creating DOM element for fun

--- DOM/modifying DOM/index.js
+++ DOM/modifying DOM/index.js
@@ -1,8 +1,15 @@
-const newPost = document.createElement("div");
-newPost.className = "top-post";
-newPost.innerHTML = "<strong>This is a new post</strong>";
+// Challenge:
+// 1. Update text in the Scrimba mini-browser to match the title of this cast
+// 2. Create an h2 with class 'tagline' and text "I can create HTML elements!"
+// Add it right under the modified text.
 
-// document.body.prepend(newPost);
-const post = document.querySelector(".post");
+// 1
+let theH1 = (document.querySelector("h1").innerHTML = "title of this cast");
 
-post.prepend(newPost);
+// 2
+let h2 = document.createElement("h2");
+let h2Text = document.createTextNode("I can create HTML elements!");
+h2.appendChild(h2Text);
+
+const getBody = document.querySelector("body");
+getBody.appendChild(h2);
