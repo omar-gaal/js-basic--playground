027277b8107bb7838684e6c693e0b68114b85cbe
omar-gaal <omargaal123@gmail.com>
2026-05-18 20:27:17 +0200

    learning about fetch()

--- Async-JavaScript/index.js
+++ Async-JavaScript/index.js
@@ -3,11 +3,15 @@
 // fulfilled
 // rejected
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => reject(Error("Promise failed.")), 1000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => reject(Error("Promise failed.")), 1000);
+// });
 
-promise
-  .then((value) => console.log(value))
-  .catch((error) => console.error(error))
-  .finally(() => console.log("done"));
+// promise
+//   .then((value) => console.log(value))
+//   .catch((error) => console.error(error))
+//   .finally(() => console.log("done"));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
+  console.log(response),
+);
