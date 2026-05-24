c04ce2964744e33b78be0d68fe74819c9ff7612a
omar-gaal <omargaal123@gmail.com>
2026-05-24 12:36:22 +0200

    learned about fetching data and place the data into a object, let goo

--- Async-JavaScript/index.js
+++ Async-JavaScript/index.js
@@ -12,6 +12,24 @@
 //   .catch((error) => console.error(error))
 //   .finally(() => console.log("done"));
 
-fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
-  console.log(response),
-);
+// fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
+//   console.log(response),
+// );
+
+// Challenge:
+// The JSON Placeholder API has /users endpoint,
+// just like the one we saw in the tutorial for /posts
+// Get the user with ID 3 and log their name and company they work for.
+// Handle errors if something does not quite work.
+
+// Here's the endpoint: https://jsonplaceholder.typicode.com/users/3
+
+fetch("https://jsonplaceholder.typicode.com/users/3")
+  .then((response) => response.json())
+  .then((data) => {
+    const newData = {
+      name: data.name,
+      company: data.company.name,
+    };
+    console.log(newData);
+  });
